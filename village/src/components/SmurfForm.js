842d651c6b570b47436abba196cad9da3ee57a92
Mona LisaHeyClos <thiscarlos@gmail.com>
2019-05-24 09:45:24 -0700

    add smurf and componentDidMount added

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
+// Not sure if i should be using 3333 or 3000 in .post
+// Do i really need componentDidMount in App.js?
+// What is res.data? will using this cause issues?
+// Will using re.data cause me to have to change 
+// input change and other stuff?
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
@@ -10,16 +16,31 @@ class SmurfForm extends Component {
     };
   }
 
-  addSmurf = event => {
+  addSmurf = (event, smurf) => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then (res => {
+        this.setState ({
+          smurfs: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
     });
   }
+
+  // addSmurf = event => {
+  //   event.preventDefault();
+  //   // add code to create the smurf using the api
+
+  //   this.setState({
+  //     name: '',
+  //     age: '',
+  //     height: ''
+  //   });
+  // }
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
